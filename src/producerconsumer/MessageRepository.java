08983818a1c6f5c4bb58745b3dc0acb9915e7862
parth2211 <parthdhorajiya365@gmail.com>
2025-07-03 00:40:59 +0530

    avoided deadlock by wait and notigy

--- src/producerconsumer/MessageRepository.java
+++ src/producerconsumer/MessageRepository.java
@@ -13,19 +13,29 @@ public class MessageRepository {
 
     public synchronized void write(String msg) {
         while(hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         this.message = msg;
         System.out.println("Thread - " + Thread.currentThread().getName() + " Written message : " + msg);
         hasMessage = true;
+        notifyAll();
     }
 
     public synchronized String read() {
         while(!hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         System.out.println("Thread - " + Thread.currentThread().getName() + " Read message : " + this.message);
         hasMessage = false;
+        notifyAll();
         return this.message;
     }
 }
